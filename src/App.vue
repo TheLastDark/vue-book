4f159f422a77cd9b0419776b134b545fad108172
TheLastDark <yinghuo68@163.com>
2019-01-24 18:23:28 +0800

    init1

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <div class="text">123</div>
+  </div>
+</template>
+<script>
+export default {
+  mounted () {
+    this.$store.dispatch('setTest', 10)
+  }
+}
+document.addEventListener('DOMContentLoaded', () => {
+  const html = document.querySelector('html')
+  let fontSize = window.innerWidth / 10
+  html.style.fontSize = fontSize + 'px'
+})
+</script>
+<style lang="scss" scoped>
+  @import './assets/styles/global';
+  .text {
+    font-size: px2rem(20);
+    color: red;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <span class="icon-bookmark"></span>
+    <span class="icon-clock"></span>
+  </div>
+</template>
+<script>
+export default {
+  name: 'home'
+}
+</script>
+<style lang="scss" scoped>
+</style>
